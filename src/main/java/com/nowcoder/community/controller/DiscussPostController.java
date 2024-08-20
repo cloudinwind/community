94400757d3933c22562b33fbd6b2f6628fef454c
cloudinwind <15729355390@139.com>
2024-08-21 00:28:13 +0800

    前三张基本功能已实现, 提交相关代码

---
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -0,0 +1,130 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.Comment;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.Page;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.CommentService;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+@Controller
+@RequestMapping("/discuss")
+public class DiscussPostController implements CommunityConstant {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CommentService commentService;
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String addDiscussPost(String title, String content) {
+        User user = hostHolder.getUser();
+        if (user == null) {
+            return CommunityUtil.getJSONString(403, "你还没有登录哦!");
+        }
+
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle(title);
+        post.setContent(content);
+        post.setCreateTime(new Date());
+        discussPostService.addDiscussPost(post);
+
+        // 报错的情况,将来统一处理.
+        return CommunityUtil.getJSONString(0, "发布成功!");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model, Page page) {
+        // 帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        // 作者
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        // 1. 评论分页信息 自定义的分页
+        page.setLimit(5);
+        page.setPath("/discuss/detail/" + discussPostId);
+        // 在帖子数据表(disscusspost 表) 中, 同时存储了该帖子下的评论数量
+        page.setRows(post.getCommentCount());
+
+        // 在帖子详情页面需要查询的评论分为两个方面: (1) 该帖子对于的评论 (2) 某个评论下的所有评论(回复)
+        // 2. 下面需要查询该帖子下的所有评论
+        // 为了使得前端页面方便显示, 使用 Map<String,Object> 进行该帖子下的评论存储
+
+        // ===首先查询该帖子下的所有评论===
+        List<Comment> commentList = commentService.findCommentsByEntity(
+                ENTITY_TYPE_POST, post.getId(), page.getOffset(), page.getLimit());
+        // 评论VO列表 存储评论信息
+        List<Map<String, Object>> commentVoList = new ArrayList<>();
+        if (commentList != null) {
+            for (Comment comment : commentList) {
+                // 评论VO
+                // 存储内容：comment 实体; user 实体; 评论下的所有回复
+                Map<String, Object> commentVo = new HashMap<>();
+                // 评论
+                commentVo.put("comment", comment);
+                // 作者
+                commentVo.put("user", userService.findUserById(comment.getUserId()));
+
+                // === 其次查询该评论下的所有回复 ===
+                List<Comment> replyList = commentService.findCommentsByEntity(
+                        ENTITY_TYPE_COMMENT, comment.getId(), 0, Integer.MAX_VALUE);
+                // 回复VO列表
+                List<Map<String, Object>> replyVoList = new ArrayList<>();
+                if (replyList != null) {
+                    for (Comment reply : replyList) {
+
+                        // 回复 Vo:
+                        // 存储内容: reply （实际上也是 comment） 实体; user 实体; target(回复对象) 实体 因为可能是 回复的回复
+                        Map<String, Object> replyVo = new HashMap<>();
+                        // 回复
+                        replyVo.put("reply", reply);
+                        // 作者
+                        replyVo.put("user", userService.findUserById(reply.getUserId()));
+                        // 回复目标
+                        // 如果reply.getTargetId() == 0, 表示是对顶层评论进行回复; 如果不等于0, 则表示是回复的回复
+                        User target = reply.getTargetId() == 0 ? null : userService.findUserById(reply.getTargetId());
+                        replyVo.put("target", target);
+
+                        replyVoList.add(replyVo);
+                    }
+                }
+                commentVo.put("replys", replyVoList);
+
+                // 回复数量
+                int replyCount = commentService.findCommentCount(ENTITY_TYPE_COMMENT, comment.getId());
+                commentVo.put("replyCount", replyCount);
+
+                commentVoList.add(commentVo);
+            }
+        }
+
+        model.addAttribute("comments", commentVoList);
+
+        return "/site/discuss-detail";
+    }
+
+}
---
+++ src/main/java/com/nowcoder/community/controller/MessageController.java
@@ -0,0 +1,154 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.Message;
+import com.nowcoder.community.entity.Page;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.MessageService;
+import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
+
+@Controller
+public class MessageController {
+
+    @Autowired
+    private MessageService messageService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
+    // 私信列表(会话列表)
+    @RequestMapping(path = "/letter/list", method = RequestMethod.GET)
+    public String getLetterList(Model model, Page page) {
+        User user = hostHolder.getUser();
+        // 分页信息
+        page.setLimit(5);
+        page.setPath("/letter/list");
+        page.setRows(messageService.findConversationCount(user.getId()));
+
+        // 会话列表
+        List<Message> conversationList = messageService.findConversations(
+                user.getId(), page.getOffset(), page.getLimit());
+        List<Map<String, Object>> conversations = new ArrayList<>();
+        if (conversationList != null) {
+            for (Message message : conversationList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("conversation", message);
+                map.put("letterCount", messageService.findLetterCount(message.getConversationId()));
+                map.put("unreadCount", messageService.findLetterUnreadCount(user.getId(), message.getConversationId()));
+
+                // targetId: 在每一个会话的 前面需要显示 和当前用户交流的那个人的头像, 因此先得到那个人的id(类似于微信的消息列表中, 每一个会话都有朋友的头像)
+                // 如果发现 消息的发送者是当前用户, 那么和当前用户交流的那个人的id就是 toId
+                // 如果发现 消息发送者是对方,  那么和当前用户交流的那个人的id就是 fromId
+                int targetId = user.getId() == message.getFromId() ? message.getToId() : message.getFromId();
+                map.put("target", userService.findUserById(targetId));
+
+                conversations.add(map);
+            }
+        }
+        model.addAttribute("conversations", conversations);
+
+        // 查询未读消息数量(因为需要查询的是该用户所有未读消息)
+        int letterUnreadCount = messageService.findLetterUnreadCount(user.getId(), null);
+        model.addAttribute("letterUnreadCount", letterUnreadCount);
+
+        return "/site/letter";
+    }
+
+    // 某一个会话中的所有消息
+    @RequestMapping(path = "/letter/detail/{conversationId}", method = RequestMethod.GET)
+    public String getLetterDetail(@PathVariable("conversationId") String conversationId, Page page, Model model) {
+        // 分页信息
+        page.setLimit(5);
+        page.setPath("/letter/detail/" + conversationId);
+        page.setRows(messageService.findLetterCount(conversationId));
+
+        // 私信列表(会话中的消息列表)
+        List<Message> letterList = messageService.findLetters(conversationId, page.getOffset(), page.getLimit());
+        // 将查询得到的消息都放入 letters 中, 便于前端显示
+        List<Map<String, Object>> letters = new ArrayList<>();
+        if (letterList != null) {
+            for (Message message : letterList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("letter", message);
+                map.put("fromUser", userService.findUserById(message.getFromId()));
+                letters.add(map);
+            }
+        }
+        model.addAttribute("letters", letters);
+
+        // 私信目标 其实就是和当前用户交流的另一个人 需要注意: conversationId 111_145 的形式
+        model.addAttribute("target", getLetterTarget(conversationId));
+
+        // 设置已读 需要获取 该会话的所有消息中 toId 不是当前用户的消息id
+        List<Integer> ids = getLetterIds(letterList);
+        if (!ids.isEmpty()) {
+            messageService.readMessage(ids);
+        }
+
+        return "/site/letter-detail";
+    }
+
+    private User getLetterTarget(String conversationId) {
+        String[] ids = conversationId.split("_");
+        int id0 = Integer.parseInt(ids[0]);
+        int id1 = Integer.parseInt(ids[1]);
+
+        if (hostHolder.getUser().getId() == id0) {
+            return userService.findUserById(id1);
+        } else {
+            return userService.findUserById(id0);
+        }
+    }
+
+    private List<Integer> getLetterIds(List<Message> letterList) {
+        List<Integer> ids = new ArrayList<>();
+
+        if (letterList != null) {
+            for (Message message : letterList) {
+                // 在该消息中 当前用户是 toId, 则表明已读
+                if (hostHolder.getUser().getId() == message.getToId() && message.getStatus() == 0) {
+                    ids.add(message.getId());
+                }
+            }
+        }
+
+        return ids;
+    }
+
+    @RequestMapping(path = "/letter/send", method = RequestMethod.POST)
+    @ResponseBody
+    public String sendLetter(String toName, String content) {
+        User target = userService.findUserByName(toName);
+        if (target == null) {
+            return CommunityUtil.getJSONString(1, "目标用户不存在!");
+        }
+
+        Message message = new Message();
+        message.setFromId(hostHolder.getUser().getId());
+        message.setToId(target.getId());
+        if (message.getFromId() < message.getToId()) {
+            message.setConversationId(message.getFromId() + "_" + message.getToId());
+        } else {
+            message.setConversationId(message.getToId() + "_" + message.getFromId());
+        }
+        message.setContent(content);
+        message.setCreateTime(new Date());
+        messageService.addMessage(message);
+
+        return CommunityUtil.getJSONString(0);
+    }
+
+}
