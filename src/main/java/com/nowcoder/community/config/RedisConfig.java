cbc90068dc3e50816a2d1fb104dc3c48a5bff3a8
cloudinwind <cloudinwind@outlook.com>
2024-08-26 23:08:01 +0800

    redis配置类

---
+++ src/main/java/com/nowcoder/community/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.nowcoder.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 设置key的序列化方式
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        // 设置alue的序列化方式
+        redisTemplate.setValueSerializer(RedisSerializer.json());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashValueSerializer(RedisSerializer.json());
+
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
